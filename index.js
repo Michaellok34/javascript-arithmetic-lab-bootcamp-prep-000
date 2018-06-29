ab201f73719cff423902462504b0682ae614e2f4
Michael Lok <michaellok34@yahoo.co.uk>
2018-06-29 18:04:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ function add (a,b, result){
   return result;
 }
 
-function subtract (a,b, result){
+function subtracts (a,b, result){
   result = a - b;
   return result;
 }
 
+function multiplies (a,b, result){
+  result = a * b;
+  return result;
+}
+
+function divides (a,b, result){
+  result = a / b;
+  return result;
+}
+
+function inc (n){
+  
+  return n +=1;
+}
